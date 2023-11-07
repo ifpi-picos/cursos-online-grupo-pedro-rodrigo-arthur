7c8c5774567f6a08ee8b30574370cbb990aaf131
Pedrohrd87 <pedrohrd87@gmail.comanode>
2023-11-07 11:47:18 -0300

    :feat:add metodos

--- src/Aluno.ts
+++ src/Aluno.ts
@@ -1,43 +1,49 @@
 class Aluno {
-    private nome: string;
-    private email: string;
-    private telefone: string;
-    private numeromatricula: string;
-    private cursos: string[] = [];
+  private nome: string;
+  private email: string;
+  private telefone: string;
+  private numeromatricula: string;
+  private cursos: string[] = [];
 
-    constructor(nome: string, email: string, telefone: string, numeromatricula: string, cursos:string[]) {
-        this.nome = nome;
-        this.email = email;
-        this.telefone = telefone;
-        this.numeromatricula = numeromatricula;
-        this.cursos = cursos
-    }
+  constructor(
+    nome: string,
+    email: string,
+    telefone: string,
+    numeromatricula: string,
+    cursos: string[]
+  ) {
+    this.nome = nome;
+    this.email = email;
+    this.telefone = telefone;
+    this.numeromatricula = numeromatricula;
+    this.cursos = cursos;
+  }
 
-    public getNome(): string {
-        return this.nome;
-    }
+  public getNome(): string {
+    return this.nome;
+  }
 
-    public getEmail(): string {
-        return this.email;
-    }
+  public getTelefone(): string {
+    return this.telefone;
+  }
 
-    public cadastrar(): void {
-        
-    }
+  getNumeromatricula(): string {
+    return this.numeromatricula;
+  }
 
-    public atualizar(): void {
-        
-    }
+  public getEmail(): string {
+    return this.email;
+  }
 
-    public visualizar(): void {
-       
-    }
+  public cadastrar(): void {}
 
-    public matricular_em(curso: string): void {
-        this.cursos.push(curso);
-    }
+  public atualizar(): void {}
 
-    public gerar_desempenho_em(curso: string): void {
-       
-    }
+  public visualizar(): void {}
+
+  public matricular_em(curso: string): void {
+    this.cursos.push(curso);
+  }
+
+  public gerar_desempenho_em(curso: string): void {}
 }
