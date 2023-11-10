19f5aa58a0b8dd6bc72765bdb0b15f16d27b9837
Pedrohrd87 <pedrohrd87@gmail.comanode>
2023-11-10 09:53:08 -0300

    feat: att classe ProfessorDAO

--- src/DAO/ProfessorDAO.ts
+++ src/DAO/ProfessorDAO.ts
@@ -1,9 +1,31 @@
 import { Professor } from "../Entidades/Professor";
+import Conexao from "./Conexao";
 import { IDAO } from "./IDAO";
 
 class ProfessorDAO implements IDAO<Professor> {
-  cadastrar(t: Professor): Promise<Professor> {
-    throw new Error("Method not implemented.");
+  private conexao: Conexao;
+
+  constructor(conexao: Conexao) {
+    this.conexao = conexao;
+  }
+
+  async cadastrar(t: Professor): Promise<Professor | null> {
+    const insert =
+      "INSERT INTO professores (nome, telefone, email) VALUES ($1, $2, $3) RETURNING *";
+
+    try {
+      const client = await Conexao.getConexao();
+      if (!client) {
+        throw new Error("Não foi possível conectar ao banco de dados");
+      }
+      const values = [t.getNome(), t.getTelefone(), t.getEmail()];
+      const res = await this.conexao.query(insert, values);
+
+      return res && res[0] ? (res[0] as Professor) : null;
+    } catch (err) {
+      console.log(err);
+      return null;
+    }
   }
   buscarTodos(): Promise<Professor[]> {
     throw new Error("Method not implemented.");
