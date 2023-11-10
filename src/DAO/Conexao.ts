330f2b871f2b612510102e99c9253636a90f6f9b
Pedrohrd87 <pedrohrd87@gmail.comanode>
2023-11-10 09:49:16 -0300

    :feat:att classe conexão

--- src/DAO/Conexao.ts
+++ src/DAO/Conexao.ts
@@ -9,16 +9,31 @@ class Conexao {
     password: "1234",
   };
 
-  private static async getConexao(): Promise<Client | null> {
+  static async getConexao(): Promise<Client | null> {
     try {
       const client = new Client(Conexao.config);
       await client.connect();
       return client;
     } catch (err) {
-      console.log(err);
+      console.error("Erro ao conectar ao banco de dados:", err);
       return null;
     } finally {
       console.log("Conexão finalizada");
+    }
+  }
+
+  async query(sql: string, valores: any[]) {
+    const client = await Conexao.getConexao();
+    if (!client) {
+      throw new Error("Não foi possível conectar ao banco de dados");
+    }
+    try {
+      const res = await client.query(sql, valores);
+      return res.rows;
+    } catch (err) {
+      console.error("Erro na conexao da consulta:", err);
+    } finally {
+      await client.end();
     }
   }
 }
