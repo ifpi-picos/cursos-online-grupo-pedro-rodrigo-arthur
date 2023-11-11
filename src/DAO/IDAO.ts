cd98f7be3a6d197b80072577d42895b33371e928
Pedrohrd87 <pedrohrd87@gmail.comanode>
2023-11-11 14:17:27 -0300

    :feat: att interface IDAO

--- src/DAO/IDAO.ts
+++ src/DAO/IDAO.ts
@@ -1,5 +1,5 @@
 export interface IDAO<T> {
-  cadastrar(t: T): Promise<T>;
+  cadastrar(t: T): Promise<T | null>;
   buscarTodos(): Promise<T[]>;
   atualizar(id: number, dados: T): Promise<T>;
   deletar(id: number): Promise<T | null>;
