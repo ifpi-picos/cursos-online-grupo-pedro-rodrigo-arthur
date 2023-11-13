49b7dbc83d4e703893e149c041faad17555461a2
Pedrohrd87 <pedrohrd87@gmail.comanode>
2023-11-13 07:16:15 -0300

    :fix:bug classe alunoDAO

--- src/DAO/AlunoDAO.ts
+++ src/DAO/AlunoDAO.ts
@@ -3,30 +3,36 @@ import Conexao from "./Conexao";
 import { IDAO } from "./IDAO";
 
 export class AlunoDAO implements IDAO<Aluno> {
-private conexao: Conexao; 
+  private conexao: Conexao;
 
-constructor(conexao: Conexao) {
-  this.conexao = conexao;
-}
-async cadastrar(t: Aluno): Promise<Aluno | null> {
-  const insert =
-    "INSERT INTO alunos (nome, telefone, email, numero da matricula) VALUES ($1, $2, $3) RETURNING *";
-
-  try {
-    const client = await Conexao.getConexao();
-    if (!client) {
-      throw new Error("Não foi possível conectar ao banco de dados");
-    }
-    // Armazenar os valores a serem inseridos
-    const values = [t.getNome(), t.getTelefone(), t.getEmail(), t.getNumeromatricula(), t.getId()];
-    // Executar a query
-    const res = await this.conexao.query(insert, values);
-
-    return res && res[0] ? (res[0] as Aluno) : null;
-  } catch (err) {
-    console.log(err);
-    return null;
+  constructor(conexao: Conexao) {
+    this.conexao = conexao;
   }
+  async cadastrar(t: Aluno): Promise<Aluno | null> {
+    const insert =
+      "INSERT INTO alunos (nome, telefone, email, matricula) VALUES ($1, $2, $3, $4) RETURNING *";
+
+    try {
+      const client = await Conexao.getConexao();
+      if (!client) {
+        throw new Error("Não foi possível conectar ao banco de dados");
+      }
+      // Armazenar os valores a serem inseridos
+      const values = [
+        t.getNome(),
+        t.getTelefone(),
+        t.getEmail(),
+        t.getNumeromatricula(),
+        t.getId(),
+      ];
+      // Executar a query
+      const res = await this.conexao.query(insert, values);
+
+      return res && res[0] ? (res[0] as Aluno) : null;
+    } catch (err) {
+      console.log(err);
+      return null;
+    }
   }
   async buscarTodos(): Promise<Aluno[]> {
     const select = "SELECT * FROM alunos";
@@ -35,9 +41,14 @@ async cadastrar(t: Aluno): Promise<Aluno | null> {
       const client = await this.conexao.query(select, []);
 
       if (client) {
-        
-        return client.map((p: { nome: string; telefone: string; email: number; id: string; numeromatricula: string }) => {
-          return new Aluno(p.nome, p.telefone, p.email, p.id);
+        return client.map((p) => {
+          return new Aluno(
+            p.nome,
+            p.telefone,
+            p.email,
+            p.numeromatricula,
+            p.id
+          );
         });
       } else {
         return [];
