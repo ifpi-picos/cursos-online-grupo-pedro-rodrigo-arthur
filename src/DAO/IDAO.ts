9302d7558acdfe34c248b046676b11e3dd69bf0f
Pedrohrd87 <pedrohrd87@gmail.com>
2023-11-13 11:06:08 -0300

    feat:add att classe IDAO

--- src/DAO/IDAO.ts
+++ src/DAO/IDAO.ts
@@ -1,6 +1,6 @@
 export interface IDAO<T> {
   cadastrar(t: T): Promise<T | null>;
   buscarTodos(): Promise<T[]>;
-  atualizar(id: number, dados: T): Promise<T>;
+  atualizar(id: number, dados: T): Promise<T | null>;
   deletar(id: number): Promise<T | null>;
 }
