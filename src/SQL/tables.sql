e3644ec1d231925537a33af39dc78f7ba3dfc9b0
Pedrohrd87 <pedrohrd87@gmail.comanode>
2023-11-15 11:02:37 -0300

    :feat: att tabelas SQL

--- src/SQL/tables.sql
+++ src/SQL/tables.sql
@@ -1,29 +1,30 @@
-CREATE TABLE Professor (
+CREATE TABLE professor (
     id SERIAL PRIMARY KEY,
     nome VARCHAR(255) NOT NULL,
-    telefone VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL
+    telefone VARCHAR(20) NOT NULL,  -- Ajustado para um possível formato de telefone
+    email VARCHAR(320) NOT NULL  -- Ajustado para o tamanho máximo de um endereço de e-mail
 );
 
-CREATE TABLE Curso (
+CREATE TABLE curso (
     id SERIAL PRIMARY KEY,
     nome VARCHAR(255) NOT NULL,
     carga_horaria INT NOT NULL,
     status VARCHAR(255) NOT NULL,
-    id_professor INT REFERENCES Professor(id)
+    id_professor INT REFERENCES professor(id)
 );
 
-CREATE TABLE Aluno (
+CREATE TABLE aluno (
     id SERIAL PRIMARY KEY,
     nome VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
-    telefone VARCHAR(255) NOT NULL
+    email VARCHAR(320) NOT NULL,  -- Ajustado para o tamanho máximo de um endereço de e-mail
+    telefone VARCHAR(20) NOT NULL  -- Ajustado para um possível formato de telefone
 );
 
-
-/* Relacionamento N:N*/
-CREATE TABLE Curso_Aluno (
-    id_curso INT REFERENCES Curso(id),
-    id_aluno INT REFERENCES Aluno(id),
+CREATE TABLE curso_aluno (
+    id_curso INT REFERENCES curso(id),
+    id_aluno INT REFERENCES aluno(id),
+    notas1 INT NOT NULL,
+    notas2 INT NOT NULL,
+    notas3 INT NOT NULL,
     PRIMARY KEY (id_curso, id_aluno)
 );
