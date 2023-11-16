de665cb4627787475cc1d83a1dfbb28cedb96046
Pedrohrd87 <pedrohrd87@gmail.comanode>
2023-11-16 11:14:53 -0300

    feat: att classe curso

--- src/Entidades/Curso.ts
+++ src/Entidades/Curso.ts
@@ -1,24 +1,20 @@
 import { StatusCurso } from "../ENUM/StatusCurso";
-import { Professor } from "./Professor";
 
 export class Curso {
   private id: number;
   private nome: string;
   private cargaHoraria: number;
   private status: StatusCurso;
-  private idProfessor: number;
 
   constructor(
     nome: string,
     cargaHoraria: number,
     status: StatusCurso,
-    idProfessor: number,
     id?: number
   ) {
     this.nome = nome;
     this.cargaHoraria = cargaHoraria;
     this.status = status;
-    this.idProfessor = idProfessor;
     this.id = id || 0;
   }
 
@@ -48,14 +44,6 @@ export class Curso {
 
   public setStatus(status: StatusCurso): void {
     this.status = status;
-  }
-
-  public getIdProfessor(): number {
-    return this.idProfessor;
-  }
-
-  public setIdProfessor(idProfessor: number): void {
-    this.idProfessor = idProfessor;
   }
 
   public toString(): string {
