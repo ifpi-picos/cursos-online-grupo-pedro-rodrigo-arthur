7e94a7d3b9573493568200e2b4afb75acf0fc6a9
Pedrohrd87 <pedrohrd87@gmail.comanode>
2023-11-17 07:21:33 -0300

    feat: att tabelas sql

--- src/SQL/tables.sql
+++ src/SQL/tables.sql
@@ -1,10 +1,14 @@
+-- Definição da Tabela Professor
 CREATE TABLE professor (
     id SERIAL PRIMARY KEY,
     nome VARCHAR(255) NOT NULL,
-    telefone VARCHAR(20) NOT NULL,  -- Ajustado para um possível formato de telefone
-    email VARCHAR(320) NOT NULL  -- Ajustado para o tamanho máximo de um endereço de e-mail
+    telefone VARCHAR(20) NOT NULL,
+    email VARCHAR(320) NOT NULL
 );
+ALTER TABLE professor
+ADD CONSTRAINT email_unique UNIQUE (email);
 
+-- Definição da Tabela Curso
 CREATE TABLE curso (
     id SERIAL PRIMARY KEY,
     nome VARCHAR(255) NOT NULL,
@@ -12,26 +16,48 @@ CREATE TABLE curso (
     status VARCHAR(255) NOT NULL
 );
 
+-- Adicionando restrição UNIQUE à coluna "nome" na tabela Curso
+ALTER TABLE curso
+ADD CONSTRAINT nome_unique UNIQUE (nome);
+
+-- Definição da Tabela Aluno
 CREATE TABLE aluno (
     id SERIAL PRIMARY KEY,
     nome VARCHAR(255) NOT NULL,
-    email VARCHAR(320) NOT NULL,  -- Ajustado para o tamanho máximo de um endereço de e-mail
-    telefone VARCHAR(20) NOT NULL,  -- Ajustado para um possível formato de telefone
+    email VARCHAR(320) NOT NULL,
+    telefone VARCHAR(20) NOT NULL,
     status VARCHAR(255) NOT NULL
 );
 
+-- Adicionando restrição UNIQUE à coluna "email" na tabela Aluno
+ALTER TABLE aluno
+ADD CONSTRAINT email_unique UNIQUE (email);
+
+
+-- Definição da Tabela Curso_Aluno
 CREATE TABLE curso_aluno (
     id_curso INT REFERENCES curso(id),
     id_aluno INT REFERENCES aluno(id),
-    notas1 INT NOT NULL,
-    notas2 INT NOT NULL,
-    notas3 INT NOT NULL,
+    nota1 INT NOT NULL,
+    nota2 INT NOT NULL,
+    nota3 INT NOT NULL,
+    media INT NOT NULL,
     PRIMARY KEY (id_curso, id_aluno)
 );
 
+-- Adicionando restrições FOREIGN KEY à tabela Curso_Aluno
+ALTER TABLE curso_aluno
+ADD CONSTRAINT fk_curso FOREIGN KEY (id_curso) REFERENCES curso(id),
+ADD CONSTRAINT fk_aluno FOREIGN KEY (id_aluno) REFERENCES aluno(id);
+
+-- Definição da Tabela Curso_Professor
 CREATE TABLE curso_professor (
     id_curso INT REFERENCES curso(id),
     id_professor INT REFERENCES professor(id),
-    nome_curso VARCHAR(255) REFERENCES curso(nome),
     PRIMARY KEY (id_curso, id_professor)
 );
+
+-- Adicionando restrições FOREIGN KEY à tabela Curso_Professor
+ALTER TABLE curso_professor
+ADD CONSTRAINT fk_curso FOREIGN KEY (id_curso) REFERENCES curso(id),
+ADD CONSTRAINT fk_professor FOREIGN KEY (id_professor) REFERENCES professor(id);
