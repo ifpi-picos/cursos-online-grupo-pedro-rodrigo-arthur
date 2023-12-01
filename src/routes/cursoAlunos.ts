ecfb687382662729f2462996b523b4333a57937c
pedroh87 <pedrohrd87@gmail.com>
2023-12-01 10:49:01 -0300

    feat: add rota para matricular alunos (incompleto)

--- src/routes/cursoAlunos.ts
+++ src/routes/cursoAlunos.ts
@@ -1,13 +1,23 @@
 import * as Express from "express";
 import { CursoAlunoServices } from "../services/CursoAlunoServices";
-const router = Express.Router();
 
+const router = Express.Router();
 const cursoAlunoServices = new CursoAlunoServices();
 
 router.get("/", async (req, res) => {
   const cursosAlunos = await cursoAlunoServices.buscarTodos();
   res.json(cursosAlunos);
 });
+
+router.get("/:id", async (req, res) => {
+  try {
+    const id = Number(req.params.id);
+    const cursoAluno = await cursoAlunoServices.buscarPorId(id);
+    res.json(cursoAluno);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+})
 
 router.post("/", async (req, res) => {
   try {
@@ -28,5 +38,35 @@ router.delete("/:id", async (req, res) => {
     res.status(400).json({ error: error.message });
   }
 });
+
+router.put("/:idCurso/:idAluno", async (req, res) => {
+  try {
+    const idCurso = Number(req.params.idCurso);
+    const idAluno = Number(req.params.idAluno);
+    const cursoAluno = req.body;
+    const cursoAlunoAtualizado = await cursoAlunoServices.atualizarCursoAluno(
+      { idCurso, idAluno },
+      cursoAluno
+    );
+    res.json(cursoAlunoAtualizado);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+})
+
+router.put("/desmatricular/:idCurso/:idAluno", async (req, res) => {
+  try {
+    const idCurso = Number(req.params.idCurso);
+    const idAluno = Number(req.params.idAluno);
+    const desmatricular = await cursoAlunoServices.desmatricular(
+      idCurso,
+      idAluno
+    );
+    res.json(desmatricular);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+})
+
 
 export default router;
