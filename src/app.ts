88d838a7e4d82b0dd8d6c38fc18969721793b232
pedroh87 <pedrohrd87@gmail.com>
2023-12-05 11:24:54 -0300

    testes

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import professores from "./routes/professores";
 import alunos from "./routes/alunos";
 import cursoAlunos from "./routes/cursoAlunos";
 import cursos from "./routes/cursos";
+import { CursoAlunoServices } from "./services/CursoAlunoServices";
 
 const app = Express();
 const cors = require('cors')
@@ -21,8 +22,18 @@ app.use("/cursoAlunos", cursoAlunos);
 app.use("/cursos", cursos);
 AppDataSource.initialize()
   .then(async () => {
-    app.listen(3000, () => {
+    app.listen(3000, async () => {
       console.log("Server started on port 3000");
+
+      const cursoAlunoServices = new CursoAlunoServices();
+      console.log(await cursoAlunoServices.quanridadeDeAlunosPorCurso(3));
+      
+      console.log(await cursoAlunoServices.porcentagemDeAlunosAprovadosPorCurso(3));
+
+      console.log(await cursoAlunoServices.mediaGeralDeAlunosPorCurso(3));
+
+      console.log(await cursoAlunoServices.porcentagemDeAlunosReprovadosPorCurso(3));
+      
     });
   })
   .catch((error) => console.log(error));
